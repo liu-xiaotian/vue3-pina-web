bcdc32800cd9f7e22e7da9ed69e77a6a0d1a7a33
晓天 <xiaotian3303@outlook.com>
2025-12-03 23:35:22 +0800

    静态资源压缩

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,21 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // 引入mock
 import { viteMockServe } from 'vite-plugin-mock'
+// 静态资源压缩
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    //配置静态资源压缩
+    viteCompression({
+      verbose: true, //是否在控制台输出压缩结果
+      threshold: 10240, //只有文件大于10kb才会被压缩
+      algorithm: 'gzip', //压缩算法
+      ext: '.gz', //生成压缩文件的后缀
+      deleteOriginFile: false //是否删除源文件
+    }),
     vueJsx(),
     vueDevTools(),
     //  配置 SVG 图标插件
