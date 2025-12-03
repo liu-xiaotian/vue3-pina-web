a0ee77d0bf52ba0063116f9e7e01592afd18f785
晓天 <xiaotian3303@outlook.com>
2025-12-03 18:46:36 +0800

    引入elementPlus 暗黑模式

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="button-example">
+    <div class="button-row">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+// 完整引入ElementPlus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 引入暗黑模式
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 import App from './App.vue'
 import router from './router'
 
@@ -9,4 +15,5 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+app.use(ElementPlus)
 app.mount('#app')
