3338ec9524d6313d7e7cc8fd85589065963a13b7
晓天 <xiaotian3303@outlook.com>
2025-12-05 11:53:20 +0800

    layout页面布局

---
+++ src/views/layout/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="app-main">main</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-main {
+  min-height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  padding: 20px;
+  box-sizing: border-box;
+}
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>首页</div>
+  <div class="wrapper">
+    <sidebar />
+    <div class="container">
+      <div class="header">
+        <navbar />
+      </div>
+      <mymain />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Mymain from '@/views/layout/components/Main.vue'
+import Navbar from '@/views/layout/components/Navbar.vue'
+import Sidebar from '@/views/layout/components/Sidebar/index.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.wrapper {
+  position: relative;
+  width: 100%;
+  width: 100%;
+}
+.header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 999;
+  width: calc(100% - 220px);
+}
+</style>
